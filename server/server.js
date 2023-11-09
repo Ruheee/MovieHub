db92bc6c26d70a76956585703137a26ecde4dba2
Rui Dantas <ruidantas55@gmail.com>
2023-11-09 12:25:18 -0500

    ensure that express server and frontend are communcating

--- server/server.js
+++ server/server.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const app = express();
+const axios = require('axios');
 const cors = require("cors");
 const PORT = 3001;
 
 //middleware
 app.use(express.json());
 app.use(cors());
+
+app.get('/api/movies', (req,res) => {
+  res.json({"users": ["Rui", "John", "Frank"]})
+})
+
 
 app.listen(PORT, () => {
   console.log(`Server has started on ${PORT}`);
