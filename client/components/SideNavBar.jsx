7bdf71db8240ca576121456f519e23cb2c832f1a
Rui Dantas <ruidantas55@gmail.com>
2023-11-15 14:42:11 -0500

    complete CSS for navbar

--- client/components/SideNavBar.jsx
+++ client/components/SideNavBar.jsx
@@ -1,16 +1,40 @@
-import React from "react";
-import  Link  from "next/link";
+import Link from "next/link";
 
-import "../styles/nav.scss"
+//import icons
+import { BiSolidCameraMovie } from "react-icons/bi";
+import { FaHome } from "react-icons/fa";
+import { PiTelevisionSimpleFill } from "react-icons/pi";
+import { MdOutlineUpcoming } from "react-icons/md";
+import { IoIosTrendingUp } from "react-icons/io";
+
+import "../styles/nav.scss";
+
 export default function SideNavBar() {
   return (
     <nav>
       <h1>Movie Hub</h1>
-      <Link href={'/'} className="link">Home</Link>
-      <Link href={'/movies'} className="link">Movies</Link>
-      <Link href={'/tv_shows'} className="link">TV Shows</Link>
-      <Link href={'/movies/trending'} className="link">Trending</Link>
-      <Link href={'/movies/upcoming'} className="link">Upcoming</Link>
+      <div className="nav--items">
+        <Link href={"/"} className="link">
+          <FaHome className="icon" />
+          <p>Home</p>
+        </Link>
+        <Link href={"/movies"} className="link">
+          <BiSolidCameraMovie className="icon" />
+          <p>Movies</p>
+        </Link>
+        <Link href={"/tv_shows"} className="link">
+          <PiTelevisionSimpleFill className="icon" />
+          <p>TV Shows</p>
+        </Link>
+        <Link href={"/movies/trending"} className="link">
+          <IoIosTrendingUp className="icon" />
+          <p>Trending</p>
+        </Link>
+        <Link href={"/movies/upcoming"} className="link">
+          <MdOutlineUpcoming className="icon" />
+          <p>Upcoming</p>
+        </Link>
+      </div>
     </nav>
   );
 }
