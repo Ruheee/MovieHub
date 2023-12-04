07e6962ffc8a302b8dbe2c153492c1d5e3d3b06e
Rui Dantas <ruidantas55@gmail.com>
2023-12-04 15:21:35 -0500

    Complete the navbar and align all items

--- client/components/SideNavBar.jsx
+++ client/components/SideNavBar.jsx
@@ -14,7 +14,48 @@ export default function SideNavBar() {
     <nav>
       <h1>MovieHub</h1>
       <div className="nav--items">
-        
+        <ul>
+          <li>
+            <Link href={"/"} className="link">
+              <div className="nav--category">
+                <FaHome className="icon" />
+                <p>Home</p>
+              </div>
+            </Link>
+          </li>
+          <li>
+            <Link href={"/movies"} className="link">
+              <div className="nav--category">
+                <BiSolidCameraMovie className="icon" />
+                <p>Movies</p>
+              </div>
+            </Link>
+          </li>
+          <li>
+            <Link href={"/tv_shows"} className="link">
+              <div className="nav--category">
+                <PiTelevisionSimpleFill className="icon" />
+                <p>TV Shows</p>
+              </div>
+            </Link>
+          </li>
+          <li>
+            <Link href={"/movies/trending"} className="link">
+              <div className="nav--category">
+                <IoIosTrendingUp className="icon" />
+                <p>Trending</p>
+              </div>
+            </Link>
+          </li>
+          <li>
+            <Link href={"/movies/upcoming"} className="link">
+              <div className="nav--category">
+                <MdOutlineUpcoming className="icon" />
+                <p>Upcoming</p>
+              </div>
+            </Link>
+          </li>
+        </ul>
       </div>
     </nav>
   );
